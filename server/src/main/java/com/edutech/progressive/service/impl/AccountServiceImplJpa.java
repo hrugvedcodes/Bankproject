3a8b20af5bb37a07f5a2115d8790b68f441d95e3
Utkarsh gupta <122604770+Shadowsweep@users.noreply.github.com>
2026-02-04 23:27:46 +0530

    Refactor CustomerService to AccountService implementation

--- server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImplJpa.java
@@ -52,49 +52,55 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.edutech.progressive.entity.Customers;
-import com.edutech.progressive.repository.CustomerRepository;
-import com.edutech.progressive.service.CustomerService;
+import com.edutech.progressive.entity.Accounts;
+import com.edutech.progressive.repository.AccountRepository;
+import com.edutech.progressive.service.AccountService;
 
 @Service
-public class CustomerServiceImplJpa implements CustomerService {
+public class AccountServiceImplJpa implements AccountService {
 
-    private final CustomerRepository custRepo;
+    private final AccountRepository accRepo;
 
-    public CustomerServiceImplJpa(CustomerRepository custRepo) {
-        this.custRepo = custRepo;
+    public AccountServiceImplJpa(AccountRepository accRepo) {
+        this.accRepo = accRepo;
     }
 
     @Override
-    public List<Customers> getAllCustomers() throws SQLException {
-        return custRepo.findAll();
+    public List<Accounts> getAllAccounts() throws SQLException {
+        return accRepo.findAll();
     }
 
     @Override
-    public Customers getCustomerById(int customerId) throws SQLException {
-        return custRepo.findByCustomerId(customerId);
+    public Accounts getAccountById(int accountId) throws SQLException {
+        return accRepo.findById(accountId).orElse(null);
     }
 
     @Override
-    public int addCustomer(Customers customers) throws SQLException {
-        return custRepo.save(customers).getCustomerId();
+    public int addAccount(Accounts accounts) throws SQLException {
+        return accRepo.save(accounts).getAccountId();
     }
 
     @Override
-    public void updateCustomer(Customers customers) throws SQLException {
-        custRepo.save(customers);
+    public void updateAccount(Accounts accounts) throws SQLException {
+        accRepo.save(accounts);
     }
 
     @Override
-    public void deleteCustomer(int customerId) throws SQLException {
-        custRepo.deleteByCustomerId(customerId);
+    public void deleteAccount(int accountId) throws SQLException {
+        accRepo.deleteById(accountId);
     }
 
     @Override
-    public List<Customers> getAllCustomersSortedByName() throws SQLException {
-        List<Customers> list = custRepo.findAll();
-        list.sort(Customers::compareTo);
+    public List<Accounts> getAllAccountsSortedByBalance() throws SQLException {
+        List<Accounts> list = accRepo.findAll();
+        list.sort(Accounts::compareTo);
         return list;
+    }
+
+    @Override
+    public List<Accounts> getAccountsByUser(int userId) throws SQLException {
+        // Not required in Day6Test
+        return accRepo.findAll();
     }
 }
 
